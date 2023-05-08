6e486f6aedec92eca5c1244f70408f91d8eef5a9
minimain <minhee001115@gmail.com>
2023-05-08 10:03:56 +0900

    first commit

---
+++ src/routes/Cj.js
@@ -0,0 +1,14 @@
+import Nav from 'component/Nav';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import'styles/cj.scss';
+
+function Cj() {
+  return (
+    <>
+    <Nav/>
+    </>
+  )
+}
+
+export default Cj
